6398deccff786064cc2cf05a5069390075e0dfac
Elvis Lin <elvislocus@gmail.com>
2024-12-11 12:04:55 +0900

    Create a initial project for Widget Layout demo

---
+++ WidgetLayout/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WidgetLayout
+//
+//  Created by Elvis Lin on 2024/12/10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
